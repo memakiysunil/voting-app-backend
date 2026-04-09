febc6b06f226f98187546743ad450968fd00074e
Sunil <memakiysunil69@gmail.com>
2026-04-09 12:07:14 +0530

    fix: use dynamic PORT in console log

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log('listening on port 8000');
+     console.log(`listening on port ${PORT}`);
 });
