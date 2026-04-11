0feafafd6eeadee298f82561fafb49b8e3a478d8
Sunil <memakiysunil69@gmail.com>
2026-04-11 13:01:27 +0530

    feat: replace loginLimiter

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 const {jwtauthmiddleware} = require('../middlewares/auth.middleware');
-const {strictLimiter,loginLimiter} = require('../middlewares/rateLimiter.middleware');
+const {strictLimiter} = require('../middlewares/rateLimiter.middleware');
 
 router.post('/signup', userController.signup);
-router.post('/login',loginLimiter, userController.login);
+router.post('/login', userController.login);
 router.get('/profile', jwtauthmiddleware,userController.getProfile);
 router.put('/profile/password', jwtauthmiddleware,strictLimiter,userController.updatePassword);
 
